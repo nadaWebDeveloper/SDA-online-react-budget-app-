6fe19d5077eccec14422dd73282065b0e4a52ada
nada <nadayahya2020@hotmail.com>
2023-10-11 11:07:02 -0700

    Done add Expense source value date

--- src/components/ExpenseFrom.tsx
+++ src/components/ExpenseFrom.tsx
@@ -1,7 +1,7 @@
-import {useState} from 'react'
+import {FormEvent , useState , ChangeEvent} from 'react'
 
 
-type Expense =
+type ExpenseBudget =
 {
   expenseSource:string;
   expenseAmount: number;
@@ -9,19 +9,51 @@ type Expense =
 }
 
 const ExpenseFrom = () => {
-  const [expense, setExpense] = useState<Expense>({
+
+  const [expense, setExpense] = useState<ExpenseBudget>({
     expenseSource: '',
     expenseAmount: 0,
     expenseDate : '', 
   });
 
+  const [expenses, setExpenses] = useState<ExpenseBudget[]>([]);
+   const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
+ {
+  const {name , value} = event.target;
+  setExpense({ ...expense, [name]:value});
+ }
    
   // const [expense,setExpense] = useState<boolean>(false);
-
-  const handleSubmit = () =>
+ 
+  const [isError,setIsError ] = useState(false);
+  const handleSubmit = (event: FormEvent) =>
   {
-  }
+    event.preventDefault();
+    if(expense.expenseSource && expense.expenseAmount && expense.expenseDate)
+    {
+      const newExpense = {
+        //name of input:value useState.name input
+        expenseSource:expense.expenseSource, 
+        expenseAmount:expense.expenseAmount,
+        expenseDate:expense.expenseDate,
+        }; 
+        setExpenses([ ...expenses, newExpense]);
+        setIsError(false);
+        console.log("change all done")
+    }
+    else
+    {
+      setIsError(true);
+      console.log("you miss input")
 
+    }
+    //to reset value
+    setExpense({
+      expenseSource: '',
+      expenseAmount: 0,
+      expenseDate : '', });
+ 
+  }
 
 
   return (
@@ -29,18 +61,25 @@ const ExpenseFrom = () => {
           <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="expenseSource">Expense Source</label>
-                <input type="text" name="expenseSource" id="expenseSource"  placeholder="Electricity bill"/>
+                <input type="text" name="expenseSource" id="expenseSource"  placeholder="Electricity bill" onChange={handleChange} value={expense.expenseSource}/>
             </div>
             <div>
                 <label htmlFor="expenseAmount">Amount of Expense</label>
-                <input type="number" name="expenseAmount" id="expenseAmount"/>
+                <input type="number" name="expenseAmount" id="expenseAmount" onChange={handleChange} value={expense.expenseAmount}/>
             </div>
             <div>
                 <label htmlFor="expenseDate">Date of Expense</label>
-                <input type="date" name="expenseDate" id="expenseDate"/>
+                <input type="date" name="expenseDate" id="expenseDate" onChange={handleChange} value={expense.expenseDate}/>
             </div>
             <button type="submit">Add Expense</button>
         </form>
+        <ul>
+          {
+            expenses.map((expense: ExpenseBudget) =>(
+            <li key={expense.expenseAmount}>{expense.expenseSource}: {expense.expenseAmount}$ On {expense.expenseDate}</li>
+            ))
+          }
+        </ul>
     </div>
   )
 }
